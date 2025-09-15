384890d82b70853c8ab09766ee88cbe5d8354bea
¨LuchooLopez¨ <Luchoolopez2810@gmail.com>
2025-09-15 19:17:28 -0300

    arreglando develop

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,12 @@
 import express from 'express';
-import { Request, Response } from 'express';
+import cors from 'cors'
 import config from './config/config';
+import { router } from './routes';
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (req: Request, res: Response) => {
-    res.send("hola mundo")
-})
+app.use(cors());
+app.use(router);
 
 app.listen(config.port, ()=> {
     console.log(`Servidor corriendo en el puerto ${config.port}`);
