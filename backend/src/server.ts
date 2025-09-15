0fa7257cafefe8230a960c89482cbeafeb6e1667
¨LuchooLopez¨ <Luchoolopez2810@gmail.com>
2025-09-15 19:45:29 -0300

    Backend conectado a la DB

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import cors from 'cors'
 import config from './config/config';
 import { router } from './routes';
+import { dbConnect } from './config/mysql';
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(router);
 
-app.listen(config.port, ()=> {
+app.listen(config.port, async()=> {
     console.log(`Servidor corriendo en el puerto ${config.port}`);
+    await dbConnect();
 })
