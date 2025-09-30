3994cbb3387d76a74e06c66a8263fe1f3deaa606
¨LuchooLopez¨ <Luchoolopez2810@gmail.com>
2025-09-30 20:13:34 -0300

    creado el schema de user con zod

--- backend/src/validations/user.schema.ts
+++ backend/src/validations/user.schema.ts
@@ -3,6 +3,6 @@ import { z } from 'zod';
 export const userSchema = z.object({
     nombre: z.string().min(2).max(100).trim(),
     email: z.string().email().max(150).toLowerCase().trim(),
-    password: z.string().min(4).max(255),
+    password: z.string().min(6).max(255),
     telefono: z.string().max(20).optional().or(z.literal('')),
 })
