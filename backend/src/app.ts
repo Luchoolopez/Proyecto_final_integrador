bc0fcbdbe029a01410f22ed9966aacdaba6e8eaf
Luchoolopez <luchoolopez2810@gmail.com>
2025-10-16 20:26:32 -0300

    configurado cloudinary

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
-//import corsConfig from "./config/cors";
+import { configureCloudinary } from "./config/cloudinary";
 import cors from 'cors';
 import { router } from "./routes";
+
+import { Request, Response } from "express";
+import {v2 as cloudianry} from 'cloudinary'
 
 export function makeApp() {
     const app = express();
@@ -11,15 +14,16 @@ export function makeApp() {
 
     // Configuración explícita de CORS
     const corsOptions = {
-        origin: 'http://localhost:5173', // El origen de tu frontend
+        origin: 'http://localhost:5173',
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Métodos permitidos
         credentials: true, // Permite el envío de cookies y cabeceras de autorización
         allowedHeaders: "Content-Type, Authorization", // Cabeceras permitidas
         optionsSuccessStatus: 204 // Para que las peticiones OPTIONS respondan con 204
     };
     app.use(cors(corsOptions));
-
     app.use(express.urlencoded({extended: true}))
+    configureCloudinary();
+
     
     // Rutas
     app.use("/api", router);
