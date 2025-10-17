d50c45ed3a0ed2189d0b9d680fee35eafcffc16f
Luchoolopez <luchoolopez2810@gmail.com>
2025-10-17 01:38:12 -0300

    Acomodado responsive del header

--- frontend/src/components/Header/header.tsx
+++ frontend/src/components/Header/header.tsx
@@ -7,15 +7,10 @@ import { ThemeToggleButton } from '../ThemeToggleButton';
 
 export const Header = () => {
     return (
-        // CAMBIOS:
-        // 1. Quitamos `bg="light"` para que Bootstrap aplique el color de fondo del tema actual.
-        // 2. Dejamos `className="header"` para nuestros estilos personalizados.
         <Navbar expand="lg" className="header" sticky="top">
             <Container fluid>
-                <Navbar.Brand href="/" className="d-none d-lg-block">Concept & Hab</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" className="d-lg-none border-0" />
-                <Navbar.Brand href="/" className="d-lg-none mobile-logo">Concept & Hab</Navbar.Brand>
-                <Navbar.Collapse id="basic-navbar-nav">
+                <div className="d-none d-lg-flex w-100 justify-content-between align-items-center">
+                    <Navbar.Brand href="/">Concept & Hab</Navbar.Brand>
                     <Nav className="mx-auto">
                         <Nav.Link href="/new"><span>NEW IN</span></Nav.Link>
                         <Nav.Link href="/sale"><span>SALE</span></Nav.Link>
@@ -24,18 +19,45 @@ export const Header = () => {
                             <NavDropdown.Item href="/men/pants">Pants</NavDropdown.Item>
                         </NavDropdown>
                         <NavDropdown title={<span>WOMEN</span>} id="women-dropdown">
-                            <NavDropdown.Item href="/women/tops">Tops</NavDropdown.Item>
+                            <NavDropdown.Item href="/women/dresses">Dresses</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav className="flex-row">
+                        <Nav.Link href="#search" className="nav-icon"><IoSearchOutline size={30} /></Nav.Link>
+                        <Nav.Link href="/login" className="nav-icon"><CiUser size={30} /></Nav.Link>
+                        <Nav.Link href="/cart" className="nav-icon"><FiShoppingCart size={30} /></Nav.Link>
+                        <div className="nav-icon d-flex align-items-center ms-2"><ThemeToggleButton /></div>
+                    </Nav>
+                </div>
+                
+                <div className="d-lg-none w-100 mobile-header-layout">
+                    <div className="mobile-header-top">
+                        <Navbar.Brand href="/">Concept & Hab</Navbar.Brand>
+                    </div>
+                    <div className="mobile-header-bottom">
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" className="border-0" />
+                        <Nav className="flex-row">
+                            <Nav.Link href="#search" className="nav-icon"><IoSearchOutline size={26} /></Nav.Link>
+                            <Nav.Link href="/login" className="nav-icon"><CiUser size={26} /></Nav.Link>
+                            <Nav.Link href="/cart" className="nav-icon"><FiShoppingCart size={26} /></Nav.Link>
+                            <div className="nav-icon d-flex align-items-center ms-2"><ThemeToggleButton /></div>
+                        </Nav>
+                    </div>
+                </div>
+
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mx-auto d-lg-none text-center pt-3">
+                         <Nav.Link href="/new"><span>NEW IN</span></Nav.Link>
+                        <Nav.Link href="/sale"><span>SALE</span></Nav.Link>
+                        <NavDropdown title={<span>MEN / NO GENDER</span>} id="men-dropdown-mobile">
+                            <NavDropdown.Item href="/men/tshirts">T-Shirts</NavDropdown.Item>
+                            <NavDropdown.Item href="/men/pants">Pants</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title={<span>WOMEN</span>} id="women-dropdown-mobile">
                             <NavDropdown.Item href="/women/dresses">Dresses</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
-                <Nav className="flex-row">
-                    <Nav.Link href="#search" className="nav-icon"><IoSearchOutline size={30} /></Nav.Link>
-                    <Nav.Link href="/login" className="nav-icon"><CiUser size={30} /></Nav.Link>
-                    <Nav.Link href="/cart" className="nav-icon"><FiShoppingCart size={30} /></Nav.Link>
-                    {/* 2. Añadimos el botón para cambiar el tema aquí */}
-                    <div className="nav-icon d-flex align-items-center ms-2"><ThemeToggleButton /></div>
-                </Nav>
             </Container>
         </Navbar>
     );
