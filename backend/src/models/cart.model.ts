c2dae875cb6eb0bced1cb7584046dcb374c105b4
LuchooLopez <luchoolopez2810@gmail.com>
2025-10-31 17:43:16 -0300

    Hecho cart model

---
+++ backend/src/models/cart.model.ts
@@ -0,0 +1,71 @@
+import { DataTypes, Model, Optional } from "sequelize";
+import { sequelize } from "../config/database";
+
+interface CartAttributes {
+    id: number;
+    usuario_id: number;
+    variante_id: number;
+    cantidad?: number;
+    fecha_agregado?: Date;
+    fecha_actualizacion?: Date;
+}
+
+type CartCreationAttributes = Optional<CartAttributes, 'id' | 'cantidad' | 'fecha_agregado' | 'fecha_actualizacion'>;
+
+export class Cart extends Model<CartAttributes, CartCreationAttributes> implements CartAttributes {
+    public id!: number;
+    public usuario_id!: number;
+    public variante_id!: number
+    public cantidad!: number;
+    public readonly fecha_agregado!: Date;
+    public readonly fecha_actualizacion!: Date;
+}
+
+Cart.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        usuario_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'usuarios',
+                key: 'id'
+            }
+        },
+        variante_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'variante_producto',
+                key: 'id'
+            }
+        },
+        cantidad: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 1,
+            validate:{
+                min: 1
+            }
+        },
+    },
+    {
+        sequelize,
+        tableName: 'carritos',
+        modelName: 'cart',
+        timestamps: true,
+        createdAt: 'fecha_agregado',
+        updatedAt: 'fecha_actualizacion',
+        indexes: [
+            {
+                unique: true,
+                fields: ['usuario_id', 'variante_id'],
+                name: 'unique_user_variant'
+            }
+        ]
+    }
+)
