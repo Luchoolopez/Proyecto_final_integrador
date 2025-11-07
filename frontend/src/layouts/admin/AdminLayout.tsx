d9a6532beccf0aad3a381f4f162ab54b3570b240
LuchooLopez <luchoolopez2810@gmail.com>
2025-11-07 20:03:01 -0300

    creado la primera parte de la vista del admin

---
+++ frontend/src/layouts/admin/AdminLayout.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { AdminSidebar } from '../../components/admin/AdminSibebar';
+// A futuro importarías tus componentes de admin aquí:
+// import { AdminHeader } from './AdminHeader';
+// import { AdminSidebar } from './AdminSidebar';
+
+export const AdminLayout = () => {
+    return (
+        <div className="admin-layout-container" style={{ display: 'flex' }}>
+
+
+
+
+                <AdminSidebar />
+
+                <Outlet />
+        
+
+        </div>
+    );
+};
