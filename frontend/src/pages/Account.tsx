6a592120746c8edff916e71f0d6620770f1aafce
LuchooLopez <luchoolopez2810@gmail.com>
2025-11-08 02:15:36 -0300

    agregado logout button en Account

--- frontend/src/pages/Account.tsx
+++ frontend/src/pages/Account.tsx
@@ -1,15 +1,16 @@
-import { Breadcrumb } from "react-bootstrap"
+import { Breadcrumb, Button } from "react-bootstrap"
 import { UserCard } from "../components/Account/UserCard/UserCard"
 import { AddressCard } from "../components/Account/AddressCard/AddressCard"
 import { Link } from "react-router-dom"
 import { PurchaseCard } from "../components/Account/PurchaseCard/PurchaseCard"
+import { useAuthContext } from "../context/AuthContext"
 
 export const Account = () => {
-    
-    return(
+    const { logout } = useAuthContext();
+    return (
         <div className="container mt-4">
             <Breadcrumb className="breadcrum-principal text-decoration-none">
-                <Breadcrumb.Item linkAs={Link} linkProps={{to:"/"}}>
+                <Breadcrumb.Item linkAs={Link} linkProps={{ to: "/" }}>
                     Inicio
                 </Breadcrumb.Item>
                 <Breadcrumb.Item active>
@@ -18,19 +19,25 @@ export const Account = () => {
             </Breadcrumb>
 
             <h1 className="text-center my-4">MI CUENTA</h1>
-            
+
             <div className="row mb-5">
-                
+
                 <div className="col-md-5 mb-5">
-                    <UserCard/>
-                    <div className="mb-4"/> 
-                    <AddressCard/>
+                    <UserCard />
+                    <div className="mb-4" />
+                    <AddressCard />
                 </div>
 
 
                 <div className="col-md-7 mb-10">
-                    <PurchaseCard/>
+                    <PurchaseCard />
                 </div>
+            </div>
+
+            <div className="mt-3 mb-5">
+                <Button variant="outline-light" className="w-10 logout-btn" onClick={logout}>
+                    Cerrar sesión
+                </Button>
             </div>
 
         </div>
