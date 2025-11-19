badb1e9127175df13685be86047046962b78e605
¨LuchooLopez¨ <Luchoolopez2810@gmail.com>
2025-11-19 00:22:05 -0300

    terminado ordermodel

--- backend/src/models/order.model.ts
+++ backend/src/models/order.model.ts
@@ -35,6 +35,82 @@ export class Order extends Model<OrderAttributes, OrderCreationAttributes> imple
     shipping_provider?: string;
     shipping_service?: string;
     tracking_number?: string;
-    shipping_cost: number;
+    shipping_cost!: number;
 }
 
+Order.init(
+    {
+        id:{
+            type:DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        numero_pedido:{
+            type:DataTypes.STRING(20),
+            unique:true,
+            allowNull:false,
+        },
+        usuario_id:{
+            type:DataTypes.INTEGER,
+            allowNull:false,
+            references:{
+                model:'usuarios',
+                key:'id',
+            },
+        },
+        direccion_id:{
+            type:DataTypes.INTEGER,
+            references:{
+                model: 'direcciones',
+                key: 'id',
+            },
+        },
+        total:{
+            type:DataTypes.DECIMAL(10,2),
+            allowNull:false,
+            validate:{
+                min: 0,
+           },
+        },
+        estado:{
+            type:DataTypes.ENUM('pendiente', 'confirmado', 'armando', 'enviado', 'entregado', 'cancelado'),
+            defaultValue: 'pendiente',
+        },
+        notas:{
+            type:DataTypes.TEXT,
+        },
+        fecha:{
+            type:DataTypes.DATE,
+            defaultValue:DataTypes.NOW,
+        },
+        fecha_envio:{
+            type:DataTypes.DATE,
+        },
+        fecha_entrega:{
+            type:DataTypes.DATE,
+        },
+        shipping_provider:{
+            type:DataTypes.STRING,
+        },
+        shipping_service:{
+            type:DataTypes.STRING,
+        },
+        tracking_number:{
+            type:DataTypes.STRING(100),
+        },
+        shipping_cost:{
+            type:DataTypes.DECIMAL(10,2),
+            defaultValue:0
+        },
+    },
+    {
+        sequelize,
+        tableName: 'pedidos',
+        modelName: 'order',
+        timestamps: true,
+        createdAt: 'fecha',
+        updatedAt: false
+    }
+);
+
+export default Order;
