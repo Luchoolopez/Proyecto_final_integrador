ca8a8463faba5dbddefd44d7e8c7664dda2979a4
agustinbarbero <agustinbarbero932@gmail.com>
2025-11-26 17:33:49 -0300

    Preparando para desplegar

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -1,9 +1,21 @@
 import cors from 'cors';
+import 'dotenv/config'; 
+
+const allowedOrigins = [
+    'http://localhost:5173',
+    process.env.CLIENT_URL 
+];
 
 const corsOptions: cors.CorsOptions = {
-    origin: [
-        'http://localhost:5173', 
-    ],
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.indexOf(origin) !== -1 || origin === process.env.CLIENT_URL) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 };
